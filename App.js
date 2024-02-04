93268d640d82126fde36a643ea62725d937cb305
Ondra T <ot199509@icloud.com>
2024-02-04 16:22:32 +0100

    Second commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
     </View>
   );
 }
